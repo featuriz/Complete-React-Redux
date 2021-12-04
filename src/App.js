fa6870cd8d463195649a8f2d751edef800d24f1c
sudhakar <featuriz@gmail.com>
2021-12-04 12:30:38 +0530

    Clean and Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header text-center bg-primary text-light py-2">
+        <h1>Welcome to React</h1>
       </header>
+      <main className="App-body py-2">
+        <p className="start">Get Started!</p>
+        <p>Welcome :)</p>
+      </main>
+      <footer className="App-footer bg-dark text-light py-5">
+        <p className="text-center">&Copy; 2021 Featuriz</p>
+      </footer>
     </div>
   );
 }
