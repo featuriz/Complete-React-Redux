60563844f931cfa986505144ba46fd5be8eb67fe
sudhakar <featuriz@gmail.com>
2021-12-04 12:54:43 +0530

    Nesting Components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+import React, { Component } from 'react';
+import Ninjas from "./Ninjas";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header text-center bg-primary text-light py-2">
-        <h1>Welcome to React</h1>
-      </header>
-      <main className="App-body py-2">
-        <p className="start">Get Started!</p>
-        <p>Welcome :)</p>
-      </main>
-      <footer className="App-footer bg-dark text-light py-5">
-        <p className="text-center">&Copy; 2021 Featuriz</p>
-      </footer>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header text-center bg-primary text-light py-2">
+          <h1>Welcome to React</h1>
+        </header>
+        <main className="App-body py-2">
+          <p className="start">Get Started!</p>
+          <p>Welcome :)</p>
+          <Ninjas />
+        </main>
+        <footer className="App-footer bg-dark text-light py-5">
+          <p className="text-center">&Copy; 2021 Featuriz</p>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component {
+    render() {
+        return (
+            <div className="ninja">
+                <div>Name: Sudhakar</div>
+                <div>Age: 31</div>
+                <div>Belt: Black</div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas
